45e6e69a625accb48daa3057a7043f5cfd94c1dd
bryanwux <bryanwu610@gmail.com>
2021-11-11 10:28:02 -0500

    complete part of the server

--- server.go
+++ server.go
@@ -1,6 +1,12 @@
 package gorpc
 
-import "gorpc/codec"
+import (
+	"encoding/json"
+	"gorpc/codec"
+	"io"
+	"log"
+	"net"
+)
 
 const MagicNumber = 0x3bef5c
 
@@ -13,3 +19,46 @@ var DefaultOption = &Option{
 	MagicNumber: MagicNumber,
 	CodecType:   codec.GobType,
 }
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+var DefaultServer = NewServer()
+
+// Accepts connections on the listener and servers the requests for each incoming connection 
+func (server *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error: ", err)
+			return
+		}
+		go server.ServeConn(conn)
+	}
+}
+
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
+
+func (server *Server) ServerConn(conn io.ReadWriteCloser) {
+	defer func() { _ = conn.Close() }()
+	var opt Option
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: options err": err) 
+		return
+	}
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
+		return 
+	}
+	f := codec.NewCodecFuncMap[opt.CodecType] 
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return 
+	}
+	server.serveCodec(f(conn))
+}
+
+
